1b8ccb9d14ceeb410d58c878f7dbd04278c46402
Lenin Alevski <lenin.huerta@oracle.com>
2016-07-26 22:03:15 -0500

    adding base64 encoding and decoding modules

---
+++ representations/base64_decode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python -B
+import base64
+import binascii
+class base64_decode:
+
+	def transform(self,input):
+		missing_padding = len(input) % 4
+		if missing_padding != 0:
+			input += b'='* (4 - missing_padding)		
+		try:
+			return base64.decodestring(input)
+		except binascii.Error:
+			return "input is not a valid base64 encode string"
+		
---
+++ representations/base64_encode.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python -B
+import base64
+class base64_encode:
+
+	def transform(self,input):
+		return base64.b64encode(input)
